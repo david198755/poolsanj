4c874b2d0436f7987a49e12e05ee19416efab591
root <root@hermes>
2026-08-26 17:53:54 +0000

    v2.4: fix signing (cached JKS keystore, keytool-generated)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "id.artin.poolsanj"
         minSdk = 24
         targetSdk = 34
-        versionCode = 8
-        versionName = "2.3"
+        versionCode = 9
+        versionName = "2.4"
     }
 
     buildTypes {
